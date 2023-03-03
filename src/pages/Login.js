fea2eff69197d5aa2650d55d369e2322c9740e1a
Pedro Trilles Machado <trillessky@gmail.com>
2023-03-03 13:02:39 -0300

    completed login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Context from '../context/Context';
 
 function Login (){
-    const [email,setEmail] = useState('');
+    const { email, setEmail } = useContext(Context)
+
     const [emailV,setEmailV] = useState(false);
     const [pwV,setPwV] = useState(false);
-    const history = useHistory();
+    const navigate = useNavigate();
 
     const handleSubmit = (event) => {
         event.preventDefault();
         localStorage.setItem('user', {email: email});
-        history.push('/');
+        navigate('/meals');
     };
 
     const emailValidation = (event) => {
