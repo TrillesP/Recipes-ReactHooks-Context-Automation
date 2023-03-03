f673ee9011f7b4a43c440941daa399fa099ec24e
Pedro Trilles Machado <trillessky@gmail.com>
2023-03-03 13:19:57 -0300

    completed App structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
+import RecipeDetails from './pages/RecipeDetails';
+import Profile from './pages/Profile';
+import TestedRecipes from './pages/TestedRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import './style.css';
 import ContextProvider from './context/ContextProvider';
@@ -13,6 +17,11 @@ export default function App() {
           <Route exact path='/' element={ <Login/> }/>
           <Route exact path='/meals' element={ <Recipes/> }/>
           <Route exact path='/drinks' element={ <Recipes/> }/>
+          <Route exact path='/meals/:id' element={ <RecipeDetails/> }/>
+          <Route exact path='/drinks/:id' element={ <RecipeDetails/> }/>
+          <Route exact path='/profile' element={ <Profile/> }/>
+          <Route exact path='/tested-recipes' element={ <TestedRecipes/> }/>
+          <Route exact path='/favorite-recipes' element={ <FavoriteRecipes/> }/>
         </Routes>
       </BrowserRouter>
     </ContextProvider>
