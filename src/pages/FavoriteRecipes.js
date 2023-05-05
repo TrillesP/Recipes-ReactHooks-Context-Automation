7bc1074bd1b16dc34cc6192539e241e551353aad
Pedro Trilles Machado <trillessky@gmail.com>
2023-05-05 18:00:01 -0300

    started deploying favorites and tested pages

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import FavoritesBody from '../components/FavoritesBody';
 
 export default function Recipes(){
     return (
         <div>
             <Header/>
+            <FavoritesBody/>
         </div>
     );
 };
--- src/pages/TestedRecipes.js
+++ src/pages/TestedRecipes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import TestedBody from '../components/TestedBody';
 
 export default function Recipes(){
     return (
         <div>
             <Header/>
+            <TestedBody/>
         </div>
     );
 };
